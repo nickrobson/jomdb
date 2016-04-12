86a048cdb70ebfa14b30e7445ad9395d36ab08f2
Nick Robson <robson.nicholas1@gmail.com>
2016-04-12 22:40:58 +1000

    Allow disabling of printing stack traces

--- src/main/java/xyz/nickr/jomdb/JavaOMDB.java
+++ src/main/java/xyz/nickr/jomdb/JavaOMDB.java
@@ -16,9 +16,15 @@ public class JavaOMDB {
     public static final Pattern IMDB_ID_PATTERN = Pattern.compile("tt[0-9]{7}");
 
     private final JOMDBRequests requests;
+    private final boolean printStackTraces;
 
     public JavaOMDB() {
+        this(true);
+    }
+
+    public JavaOMDB(boolean printStackTraces) {
         this.requests = new JOMDBRequests();
+        this.printStackTraces = printStackTraces;
     }
 
     public JOMDBRequests getRequests() {
@@ -29,10 +35,12 @@ public class JavaOMDB {
         try {
             return requests.getJSON(params);
         } catch (JSONException ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             throw new JOMDBException("Invalid JSON", ex);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             throw new JOMDBException("Something went wrong", ex);
         }
     }
@@ -43,7 +51,8 @@ public class JavaOMDB {
             query.put("s", search);
             return new SearchResults(this, query, get(query));
         } catch (JOMDBException ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             return null;
         }
     }
@@ -59,7 +68,8 @@ public class JavaOMDB {
             query.put("plot", fullPlot ? "full" : "short");
             return new TitleResult(this, get(query));
         } catch (JOMDBException ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             return null;
         }
     }
@@ -75,7 +85,8 @@ public class JavaOMDB {
             query.put("plot", fullPlot ? "full" : "short");
             return new TitleResult(this, get(query));
         } catch (JOMDBException ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             return null;
         }
     }
@@ -87,7 +98,8 @@ public class JavaOMDB {
             query.put("Season", season);
             return new SeasonResult(this, get(query));
         } catch (JOMDBException ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             return null;
         }
     }
@@ -99,7 +111,8 @@ public class JavaOMDB {
             query.put("Season", season);
             return new SeasonResult(this, get(query));
         } catch (JOMDBException ex) {
-            ex.printStackTrace();
+            if (printStackTraces)
+                ex.printStackTrace();
             return null;
         }
     }
