f037b9c618407ce4ab9ba4fb04f1457827ae6afa
Nick Robson <robson.nicholas1@gmail.com>
2016-10-04 19:40:15 +1100

    jomdb 3.0.0 :rocket:

--- src/main/java/xyz/nickr/jomdb/JavaOMDB.java
+++ src/main/java/xyz/nickr/jomdb/JavaOMDB.java
@@ -11,27 +11,55 @@ import xyz.nickr.jomdb.model.SearchResults;
 import xyz.nickr.jomdb.model.SeasonResult;
 import xyz.nickr.jomdb.model.TitleResult;
 
+/**
+ * The API entry point.
+ */
 public class JavaOMDB {
 
+    /**
+     * A regular expression matching valid IMDB IDs.
+     */
     public static final Pattern IMDB_ID_PATTERN = Pattern.compile("tt[0-9]{7}");
 
     private final JOMDBRequests requests;
     private final boolean printStackTraces;
 
+    /**
+     * Creates an instance that will print stack traces.
+     */
     public JavaOMDB() {
         this(true);
     }
 
+    /**
+     * Creates an instance that may print stack traces.
+     *
+     * @param printStackTraces Whether or not to print stack traces.
+     */
     public JavaOMDB(boolean printStackTraces) {
         this.requests = new JOMDBRequests();
         this.printStackTraces = printStackTraces;
     }
 
+    /**
+     * Gets the corresponding {@link JOMDBRequests} object.
+     *
+     * @return The requests object.
+     */
     public JOMDBRequests getRequests() {
         return requests;
     }
 
-    public JSONObject get(Map<String, String> params) {
+    /**
+     * Queries the OMDB API with given query parameters.
+     *
+     * @param params The parameters.
+     *
+     * @return The JSON object describing the query result.
+     *
+     * @throws JOMDBException when an error occurs while retrieving or parsing JSON
+     */
+    public JSONObject get(Map<String, String> params) throws JOMDBException {
         try {
             return requests.getJSON(params);
         } catch (JSONException ex) {
@@ -45,22 +73,45 @@ public class JavaOMDB {
         }
     }
 
+    /**
+     * Searches for titles with specified text.
+     *
+     * @param search The search text.
+     *
+     * @return The search results.
+     */
     public SearchResults search(String search) {
         try {
             Map<String, String> query = new HashMap<>();
             query.put("s", search);
             return new SearchResults(this, query, get(query));
         } catch (JOMDBException ex) {
-            if (printStackTraces)
-                ex.printStackTrace();
+            if (!printStackTraces)
+                throw ex;
+            ex.printStackTrace();
             return null;
         }
     }
 
+    /**
+     * Gets a title by its name, with a short plot.
+     *
+     * @param title The title's name.
+     *
+     * @return The title.
+     */
     public TitleResult titleByName(String title) {
         return titleByName(title, false);
     }
 
+    /**
+     * Gets a title by its name, optionally with a full plot.
+     *
+     * @param title The title's name.
+     * @param fullPlot Whether or not to get the full plot.
+     *
+     * @return The title.
+     */
     public TitleResult titleByName(String title, boolean fullPlot) {
         try {
             Map<String, String> query = new HashMap<>();
@@ -68,16 +119,32 @@ public class JavaOMDB {
             query.put("plot", fullPlot ? "full" : "short");
             return new TitleResult(this, get(query));
         } catch (JOMDBException ex) {
-            if (printStackTraces)
-                ex.printStackTrace();
+            if (!printStackTraces)
+                throw ex;
+            ex.printStackTrace();
             return null;
         }
     }
 
+    /**
+     * Gets a title by its IMDB ID, with a short plot.
+     *
+     * @param imdbId The title's IMDB ID.
+     *
+     * @return The title.
+     */
     public TitleResult titleById(String imdbId) {
         return titleById(imdbId, false);
     }
 
+    /**
+     * Gets a title by its IMDB ID, optionally with a full plot.
+     *
+     * @param imdbId The title's IMDB ID.
+     * @param fullPlot Whether or not to get the full plot.
+     *
+     * @return The title.
+     */
     public TitleResult titleById(String imdbId, boolean fullPlot) {
         try {
             Map<String, String> query = new HashMap<>();
@@ -85,12 +152,21 @@ public class JavaOMDB {
             query.put("plot", fullPlot ? "full" : "short");
             return new TitleResult(this, get(query));
         } catch (JOMDBException ex) {
-            if (printStackTraces)
-                ex.printStackTrace();
+            if (!printStackTraces)
+                throw ex;
+            ex.printStackTrace();
             return null;
         }
     }
 
+    /**
+     * Gets a season by title name and season number.
+     *
+     * @param title The title name.
+     * @param season The season number.
+     *
+     * @return The season.
+     */
     public SeasonResult seasonByName(String title, String season) {
         try {
             Map<String, String> query = new HashMap<>();
@@ -98,12 +174,21 @@ public class JavaOMDB {
             query.put("Season", season);
             return new SeasonResult(this, get(query));
         } catch (JOMDBException ex) {
-            if (printStackTraces)
-                ex.printStackTrace();
+            if (!printStackTraces)
+                throw ex;
+            ex.printStackTrace();
             return null;
         }
     }
 
+    /**
+     * Gets a season by title's IMDB ID and season number.
+     *
+     * @param imdbId The title's IMDB ID.
+     * @param season The season number.
+     *
+     * @return The season.
+     */
     public SeasonResult seasonById(String imdbId, String season) {
         try {
             Map<String, String> query = new HashMap<>();
@@ -111,8 +196,9 @@ public class JavaOMDB {
             query.put("Season", season);
             return new SeasonResult(this, get(query));
         } catch (JOMDBException ex) {
-            if (printStackTraces)
-                ex.printStackTrace();
+            if (!printStackTraces)
+                throw ex;
+            ex.printStackTrace();
             return null;
         }
     }
--- src/main/java/xyz/nickr/jomdb/model/SearchResultsPage.java
+++ src/main/java/xyz/nickr/jomdb/model/SearchResultsPage.java
@@ -13,6 +13,9 @@ import org.json.JSONObject;
 import lombok.Getter;
 import xyz.nickr.jomdb.JOMDBException;
 
+/**
+ * Represents a single page of {@link SearchResults search results}.
+ */
 public class SearchResultsPage implements Iterable<SearchResult> {
 
     @Getter
@@ -42,6 +45,13 @@ public class SearchResultsPage implements Iterable<SearchResult> {
         }
     }
 
+    /**
+     * Gets a result from this page.
+     *
+     * @param x The search result index of this page, must be in {@code [0, size())}.
+     *
+     * @return The search result.
+     */
     public SearchResult getResult(int x) {
         if (x < 0 || x >= this.res.length) {
             throw new IllegalArgumentException(String.format("(%d) is not in range [0,%d)", x, this.res.length));
@@ -49,6 +59,11 @@ public class SearchResultsPage implements Iterable<SearchResult> {
         return this.res[x];
     }
 
+    /**
+     * Gets the amount of {@link SearchResult search results} on this page.
+     *
+     * @return The number of results.
+     */
     public int size() {
         return this.res.length;
     }
@@ -63,6 +78,11 @@ public class SearchResultsPage implements Iterable<SearchResult> {
         return Spliterators.spliterator(this.iterator(), this.res.length, Spliterator.ORDERED | Spliterator.NONNULL | Spliterator.SIZED);
     }
 
+    /**
+     * Gets the results on this page as a stream.
+     *
+     * @return The stream.
+     */
     public Stream<SearchResult> stream() {
         return StreamSupport.stream(this.spliterator(), false);
     }
